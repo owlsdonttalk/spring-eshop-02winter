f21c07ba17103293e68fe4145a5f36e2592e063d
i.petrov <ivan@miropolskiy.com>
2022-02-21 22:33:43 +0300

    first commit

---
+++ shop-backend-api-app/src/main/java/ru/geekbrains/GeekShopBackendApiAppApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekShopBackendApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekShopBackendApiAppApplication.class, args);
+    }
+
+}
